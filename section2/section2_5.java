8e12b81da0136ca7bba6aece0fe09074607ae876
jyoungjin <ccd1202@naver.com>
2022-03-30 22:22:46 +0900

    section 2-5 에라토스테네스 체 해결

--- section2/section2_5.java
+++ section2/section2_5.java
@@ -1,13 +1,24 @@
 package section2;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /* 5. 소수(에라토스테네스 체) */
 public class section2_5 {
+
     public int solution(int n) {
 
         int answer = 0;
+
+        int[] arr = new int[n+1];
+
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j = j + i) {
+                    arr[j] = 1;
+                }
+            }
+        }
 
 
         return answer;
